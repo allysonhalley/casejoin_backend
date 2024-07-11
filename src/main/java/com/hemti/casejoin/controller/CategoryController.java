d0b73613e03b6b7bf6497ef413dc84ba757126de
allysonhalley <allysonhalley@gmail.com>
2024-07-10 23:02:39 -0300

    add CrossOrigin

--- src/main/java/com/hemti/casejoin/controller/CategoryController.java
+++ src/main/java/com/hemti/casejoin/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
